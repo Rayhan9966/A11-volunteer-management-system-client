2d2aa46f5a15a87151af7ffa4bae145f724e25c9
Rayhan9966 <rayhancse162gmail.com>
2024-05-13 01:05:42 +0400

    connected server with client site

--- src/Pages/AddVolunteer.jsx
+++ src/Pages/AddVolunteer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AddVolunteer = () => {
     return (
         <div>
-           <h1>addddddddddddded</h1> 
+           <h1>addddddddddddded form ready hbe </h1> 
         </div>
     );
 };
