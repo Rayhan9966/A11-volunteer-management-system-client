6c13cd40cb7cee94522673cf5cacb3a8b918e39c
Rayhan9966 <rayhancse162gmail.com>
2024-05-13 19:47:24 +0400

    delete btn updated

--- src/Pages/AddVolunteerCard.jsx
+++ src/Pages/AddVolunteerCard.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const AddVolunteerCard = ({post}) => {
-    const { name,thumbnail,title,description, category,location, volunteerneed,deadline,email } = post;
+    const { _id,name,thumbnail,title,description, category,location, volunteerneed,deadline,email } = post;
+    const handleDelete=_id=>{
+console.log(_id);
+
+Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+    //   Swal.fire({
+    //     title: "Deleted!",
+    //     text: "Your file has been deleted.",
+    //     icon: "success"
+    //   });
+    console.log('dlt cnfrm');
+    }
+  });
+    }
     return (
         <div>
             <div className="card card-side bg-base-100 text-white dark:bg-gray-800 container ml-4 mr-4 px-4 mx-auto shadow-xl">
@@ -14,7 +37,9 @@ const AddVolunteerCard = ({post}) => {
 
    
     <div className="join join-vertical  gap-4 lg:join-horizontal">
-  <button className="btn btn-secondary join-item">Delete</button>
+  <button onClick={()=>handleDelete(_id)} className="btn btn-secondary join-item">Delete</button>
+
+
   <button className="btn btn-primary join-item">Update</button>
   <button className="btn btn-warning join-item">Apply</button>
 </div>
